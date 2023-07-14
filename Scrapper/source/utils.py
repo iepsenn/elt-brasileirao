10b224f0f2c6bf1315c52aae4d3f72e7bad1beb7
iepsenn <lukasiepsen@gmail.com>
2023-07-14 00:54:59 +0000

    add refactoring in extraction files

---
+++ Scrapper/source/utils.py
@@ -0,0 +1,26 @@
+BUCKET_NAME = "brasileirao-data"
+
+
+def normalize_string(string: str) -> str:
+    import re
+    from unidecode import unidecode
+
+    return unidecode(re.sub(r"[^\w\s]", "", string))\
+        .lower()\
+        .strip()\
+        .replace(" ", "")
+
+
+def build_data_lake_path(
+    date: str, home: str, away: str, week: int, filename: str
+) -> str:
+    import os
+
+    return os.path.join(
+        BUCKET_NAME,
+        "raw",
+        str(week),
+        date.replace("-", ""),
+        normalize_string(home) + "_" + normalize_string(away),
+        filename,
+    )
